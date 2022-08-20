3fd56fe304de16d368ae41ee207fb3cbd086ee80
Kridsanapong Buathongjun <inwpbmak@gmail.com>
2022-08-20 15:02:47 +0700

    Remove .env

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,23 @@
 <script>
   import "../app.css";
+  import axios from "axios";
+  import { onMount } from "svelte";
+
+  var discord_start = (function () {
+    var executed = false;
+    return async function () {
+      if (!executed) {
+        executed = true;
+        await axios.post(`/discord_start`).then(async function (response) {
+          console.log("nice", response);
+        });
+      }
+    };
+  })();
+  onMount(async () => {
+    console.log("staet");
+    discord_start();
+  });
 </script>
 
 <slot />
